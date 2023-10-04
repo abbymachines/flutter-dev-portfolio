2f65519d1399a106784c98a5d909ea96204d72dd
abbymachines <abby.machines@gmail.com>
2023-10-03 18:35:34 -0700

    implement longer list to demonstrate ListView is scrollable

--- lib/widgets/items/item_list.dart
+++ lib/widgets/items/item_list.dart
@@ -6,7 +6,11 @@ const items = [
   'the return of item',
   'the item strikes back',
   'infinity item',
-  'item begins'
+  'item begins',
+  'i cant believe its not item',
+  'arent you happy i didnt say item',
+  'items are nice items are cool',
+  'hello hello hello'
 ];
 
 class ItemList extends StatelessWidget {
