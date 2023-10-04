20f10520a7a8822bbfe37d7e75693e954e489fb4
abbymachines <abby.machines@gmail.com>
2023-10-03 18:59:01 -0700

    wrap Item Card in Inkwell, add print statement to onTap

--- lib/widgets/items/item.dart
+++ lib/widgets/items/item.dart
@@ -7,28 +7,31 @@ class Item extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Card(
-      color: Colors.grey,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              label,
-              style: Theme.of(context).textTheme.titleLarge,
-              textAlign: TextAlign.justify,
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const Spacer(),
-                Text(label),
-                const Spacer(),
-              ],
-            ),
-          ],
+    return InkWell(
+      onTap: () => print(label),
+      child: Card(
+        color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                label,
+                style: Theme.of(context).textTheme.titleLarge,
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Spacer(),
+                  Text(label),
+                  const Spacer(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
