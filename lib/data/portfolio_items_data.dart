0682e667861ee4152b2b135d5da5ddd66f40c971
abbymachines <abby.machines@gmail.com>
2023-10-06 16:20:01 -0700

    refactor item list data into its own file & create basic PortfolioItem model

---
+++ lib/data/portfolio_items_data.dart
@@ -0,0 +1,11 @@
+const items = [
+  'item',
+  'the return of item',
+  'the item strikes back',
+  'infinity item',
+  'item begins',
+  'i cant believe its not item',
+  'arent you happy i didnt say item',
+  'items are nice items are cool',
+  'hello hello hello'
+];
---
+++ lib/models/portfolio_item.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter/material.dart';
+
+class PortfolioItem {
+  PortfolioItem({
+    required this.label,
+    required this.expandedText,
+    key,
+  });
+
+  final String label;
+  final String expandedText;
+}
