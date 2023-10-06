7daf935ea73720c877fe23b60a85a9642c3f1257
abbymachines <abby.machines@gmail.com>
2023-10-06 16:39:07 -0700

    create portfolio data

--- lib/data/portfolio_items_data.dart
+++ lib/data/portfolio_items_data.dart
@@ -1,11 +1,25 @@
-const items = [
-  'item',
-  'the return of item',
-  'the item strikes back',
-  'infinity item',
-  'item begins',
-  'i cant believe its not item',
-  'arent you happy i didnt say item',
-  'items are nice items are cool',
-  'hello hello hello'
+import 'package:flutter_dev_portfolio/models/portfolio_item.dart';
+
+final items = [
+  PortfolioItem(label: 'item', expandedText: 'who woulda thunk'),
+  PortfolioItem(
+      label: 'item',
+      expandedText:
+          'the exciting followup to that Item you just scrolled past'),
+  PortfolioItem(
+      label: 'the item strikes back', expandedText: 'we thought it was gone'),
+  PortfolioItem(label: 'infinity item', expandedText: 'end item'),
+  PortfolioItem(
+      label: 'item begins',
+      expandedText: 'the gritty dark reboot of the item you know and love'),
+  PortfolioItem(
+      label: 'i cant believe its not item', expandedText: 'item? unlikely'),
+  PortfolioItem(
+      label: 'arent you happy i didnt say item',
+      expandedText: 'Q: KNOWCK KNOCK A: who care'),
+  PortfolioItem(
+      label: 'items are nice items are cool',
+      expandedText: 'i love to carry around whenevere im in school'),
+  PortfolioItem(
+      label: 'hello hello hello', expandedText: 'GOODBYE GOODBYE GOODBYE'),
 ];
--- lib/models/portfolio_item.dart
+++ lib/models/portfolio_item.dart
@@ -1,12 +1,15 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
 
 class PortfolioItem {
   PortfolioItem({
     required this.label,
     required this.expandedText,
-    key,
-  });
+  }) : key = uuid.v4();
 
   final String label;
   final String expandedText;
+  final String key;
 }
