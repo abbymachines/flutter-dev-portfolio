54d04e52dbbbb6d0c9248ff1c8f1025312742382
abbymachines <abby.machines@gmail.com>
2023-10-10 18:22:55 -0700

    implement basic blog models

---
+++ lib/models/blog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dev_portfolio/models/blog_post.dart';
+
+class Blog {
+  Blog({
+    required this.posts,
+  });
+
+  final List<BlogPost> posts;
+}
---
+++ lib/models/blog_post.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class BlogPost {
+  BlogPost({
+    required this.posts,
+  });
+
+  final List posts;
+}
