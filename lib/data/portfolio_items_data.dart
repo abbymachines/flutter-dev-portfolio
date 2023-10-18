df6b8ad8ad1fc65d064f36d756088080375b922f
abbymachines <abby.machines@gmail.com>
2023-10-18 15:44:53 -0700

    restore portfolio items

--- lib/data/portfolio_items_data.dart
+++ lib/data/portfolio_items_data.dart
@@ -4,41 +4,33 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 
 final items = [
   GenericItem(
-    label: 'markdown test :3',
-    expandedText: 'shout out to web development',
-  ),
+      label:
+          'Hello! :) My name is Abby Castillo. I am a software developer and interdisciplinary artist based in Portland, OR.',
+      expandedText:
+          'I have several websites you can look at and also I have a linkedin and such. Lorem ipso facto badabing badaboom and etcetera as you know',
+      color: Color.fromARGB(255, 255, 195, 15),
+      style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+  GenericItem(label: 'Habit Pet', expandedText: 'who woulda thunk'),
+  GenericItem(
+      label: 'A',
+      expandedText:
+          "'A' is a series of AR videos made with Meta Spark Studio."),
+  GenericItem(
+      label: 'Skills',
+      expandedText:
+          'Flutter / React / Kotlin(?) / Flask / SQL / REST APIs / full-stack development'),
+  GenericItem(label: 'infinity item', expandedText: 'end item'),
+  GenericItem(
+      label: 'item begins',
+      expandedText: 'the gritty dark reboot of the item you know and love'),
+  GenericItem(
+      label: 'i cant believe its not item', expandedText: 'item? unlikely'),
+  GenericItem(
+      label: 'arent you happy i didnt say item',
+      expandedText: 'Q: KNOWCK KNOCK A: who care'),
+  GenericItem(
+      label: 'items are nice items are cool',
+      expandedText: 'i love to carry around whenevere im in school'),
+  GenericItem(
+      label: 'hello hello hello', expandedText: 'GOODBYE GOODBYE GOODBYE'),
 ];
-
-
-// final items = [
-//   GenericItem(
-//       label:
-//           'Hello! :) My name is Abby Castillo. I am a software developer and interdisciplinary artist based in Portland, OR.',
-//       expandedText:
-//           'I have several websites you can look at and also I have a linkedin and such. Lorem ipso facto badabing badaboom and etcetera as you know',
-//       color: Color.fromARGB(255, 255, 195, 15),
-//       style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-//   GenericItem(label: 'Habit Pet', expandedText: 'who woulda thunk'),
-//   GenericItem(
-//       label: 'A',
-//       expandedText:
-//           "'A' is a series of AR videos made with Meta Spark Studio."),
-//   GenericItem(
-//       label: 'Skills',
-//       expandedText:
-//           'Flutter / React / Kotlin(?) / Flask / SQL / REST APIs / full-stack development'),
-//   GenericItem(label: 'infinity item', expandedText: 'end item'),
-//   GenericItem(
-//       label: 'item begins',
-//       expandedText: 'the gritty dark reboot of the item you know and love'),
-//   GenericItem(
-//       label: 'i cant believe its not item', expandedText: 'item? unlikely'),
-//   GenericItem(
-//       label: 'arent you happy i didnt say item',
-//       expandedText: 'Q: KNOWCK KNOCK A: who care'),
-//   GenericItem(
-//       label: 'items are nice items are cool',
-//       expandedText: 'i love to carry around whenevere im in school'),
-//   GenericItem(
-//       label: 'hello hello hello', expandedText: 'GOODBYE GOODBYE GOODBYE'),
-// ];
