9add81028ff116da3c00e242cba74b7a89653328
abbymachines <abby.machines@gmail.com>
2023-10-18 13:43:49 -0700

    write blog post

--- lib/data/blog_posts.dart
+++ lib/data/blog_posts.dart
@@ -4,9 +4,19 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 
 final blogPostData = [
   BlogPost(
+      date: '2023.10.18',
+      body: const MarkdownBody(
+          data:
+              """How exciting! Now that I have my MarkdownBody widgets properly configured, I can begin my blog in earnest. So hello there. :) Hello world. How\'s it going? :)
+              
+              This is my developer blog. This is a space for me to document the day-by-day process of building my developer portfolio, and other software development projects.
+
+              I'm still not entirely sure how to format paragraphs with the way my blog is set up. But I'm learning! Everything is coming along.
+              """)),
+  BlogPost(
     date: '2023.10.17',
     body: const MarkdownBody(
-      data: 'believe it or not, george isnt at home',
+      data: 'believe it or not, **george** isnt at home',
     ),
   ),
   BlogPost(
