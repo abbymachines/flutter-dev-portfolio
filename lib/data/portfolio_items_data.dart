e9e3ce1a19edf3e56aa6087354ab28e3f2d293a6
abbymachines <abby.machines@gmail.com>
2023-10-19 14:44:39 -0700

    edit portfolio data

--- lib/data/portfolio_items_data.dart
+++ lib/data/portfolio_items_data.dart
@@ -10,7 +10,15 @@ final items = [
           'I have several websites you can look at and also I have a linkedin and such. Lorem ipso facto badabing badaboom and etcetera as you know',
       color: Color.fromARGB(255, 255, 195, 15),
       style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-  GenericItem(label: 'Habit Pet', expandedText: 'who woulda thunk'),
+  GenericItem(
+      label: 'Habit Pet',
+      expandedText:
+          ''''**Wurm** is a habit tracker attached to a tamagotchi-style virtual pet. Currently in development using Flutter for Android, iOS, and the web.
+
+The app allows you to keep a list of habits and track their completion over time.
+
+When habits are incomplete, the pet gets hungry and sick. Completing habits feeds the pet and makes it happier.'''),
+  GenericItem(label: 'Simple Pet', expandedText: 'lol'),
   GenericItem(
       label: 'A',
       expandedText:
@@ -19,18 +27,4 @@ final items = [
       label: 'Skills',
       expandedText:
           'Flutter / React / Kotlin(?) / Flask / SQL / REST APIs / full-stack development'),
-  GenericItem(label: 'infinity item', expandedText: 'end item'),
-  GenericItem(
-      label: 'item begins',
-      expandedText: 'the gritty dark reboot of the item you know and love'),
-  GenericItem(
-      label: 'i cant believe its not item', expandedText: 'item? unlikely'),
-  GenericItem(
-      label: 'arent you happy i didnt say item',
-      expandedText: 'Q: KNOWCK KNOCK A: who care'),
-  GenericItem(
-      label: 'items are nice items are cool',
-      expandedText: 'i love to carry around whenevere im in school'),
-  GenericItem(
-      label: 'hello hello hello', expandedText: 'GOODBYE GOODBYE GOODBYE'),
 ];
