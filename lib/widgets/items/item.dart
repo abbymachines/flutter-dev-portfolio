ef5bc9638d89c746c2fc9bf7a5be2476daaddeb3
abbymachines <abby.machines@gmail.com>
2023-10-24 14:48:26 -0700

    re-implement Item Card stylings

--- lib/widgets/items/item.dart
+++ lib/widgets/items/item.dart
@@ -34,60 +34,50 @@ class _ItemState extends State<Item> {
   Widget build(BuildContext context) {
     return InkWell(
         onTap: toggleExpanded,
-        // child: Card(
-        //   color: widget.item.color,
-        //   child: Padding(
-        //     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        //     child: Column(
-        //       crossAxisAlignment: CrossAxisAlignment.start,
-        //       mainAxisSize: MainAxisSize.min,
-        //       children: [
-        //         Text(
-        //           widget.item.label,
-        //           style: widget.item.style,
-        //           textAlign: TextAlign.left,
-        //         ),
-        //         Text(
-        //           widget.item.mainText,
-        //           style: const TextStyle(
-        //               fontSize: 20,
-        //               fontStyle: FontStyle.italic,
-        //               color: Colors.white),
-        //         ),
-        //         const SizedBox(height: 4),
-        //         Row(
-        //           children: [
-        //             Flexible(
-        //               fit: FlexFit.loose,
-        //               child: MarkdownBody(data: _expandedContent),
-        //             ),
-        //             const Spacer(),
-        //           ],
-        //         ),
-        //         Image.asset(
-        //           Images.sproutling,
-        //           width: 50,
-        //           height: 50,
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
+        // child:
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(
+            SizedBox(
               width: 225,
               child: Card(
-                child: Column(
-                  children: [
-                    Text(
-                      'hello Card row 2',
-                    ),
-                    Text(
-                      'hello Card row 1! AKA the top of the column',
-                    ),
-                  ],
+                color: widget.item.color,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        widget.item.label,
+                        style: widget.item.style,
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        widget.item.mainText,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: [
+                          Flexible(
+                            fit: FlexFit.loose,
+                            child: MarkdownBody(data: _expandedContent),
+                          ),
+                          const Spacer(),
+                        ],
+                      ),
+                      Image.asset(
+                        Images.sproutling,
+                        width: 50,
+                        height: 50,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
