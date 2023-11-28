38c33e265bef518b22c6aea2809f5063d6132183
abbymachines <abby.machines@gmail.com>
2023-11-28 15:26:25 -0800

    update portfolio content

--- lib/data/portfolio_items_data.dart
+++ lib/data/portfolio_items_data.dart
@@ -8,9 +8,8 @@ final items = [
     label: 'Hello! :) My name is Abby Castillo.',
     mainText: "it's true i think!",
     expandedText: '''
-  I am a software developer and interdisciplinary artist based in Portland, OR.
-        
-  I have several websites you can look at and also I have a linkedin and such. Lorem ipso facto badabing badaboom and etcetera as you know''',
+  I am a full-stack software developer and interdisciplinary artist based in Portland, OR. I am **currently looking for work!**
+''',
     color: const Color.fromARGB(255, 255, 195, 15),
     style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
     image: Images.iconA0Small,
@@ -20,7 +19,7 @@ final items = [
       mainText:
           'focusing on full-stack web development and mobile app development',
       expandedText: '''
-*LANGUAGES*:
+## **LANGUAGES**:
 
   • Python
 
@@ -40,7 +39,7 @@ final items = [
   
   • P5
           
-*FRONT-END*: 
+## **FRONT-END**: 
 
 • React.js
 
@@ -50,7 +49,7 @@ final items = [
 
 • Flutter
           
-*OTHER SKILLS*:
+## **OTHER SKILLS**:
 
 • REST APIs
 
@@ -72,32 +71,34 @@ final items = [
   GenericItem(
       label: 'A (AR video series)',
       expandedText:
-          "'A' is a series of AR videos made with Meta Spark Studio."),
+          "'A' is a series of AR videos made with Meta Spark Studio. Part of a 2023 Precipice Fund grant. Videos online at [TikTok](https://www.tiktok.com/@abbymachines)."),
   GenericItem(
       label: 'Tama Habits',
       expandedText:
-          ''''**Tama Habits** is a habit tracker attached to a tamagotchi-style virtual pet. Currently in development using Flutter for Android, iOS, and the web.
+          ''''**Tama Habits** is a habit tracker attached to a tamagotchi-style virtual pet. A demo of this app was first developed as my capstone project at the Ada Developers Academy. It is currently in development using **Flutter** for **Android**, **iOS**, and the **web**.
 
 The app allows you to keep a list of habits and track their completion over time.
 
 When habits are incomplete, the pet gets hungry and sick. Completing habits feeds the pet and makes it happier.
 
-A demo of this app will be available soon (November 2023) on this website as **Tama To-Do**. This demo will include limited features, functioning as a to-do list instead of a habit tracker.
+A public demo of this app will be available soon (November 2023) on this website as **Tama To-Do**. This demo will include limited features, functioning as a to-do list instead of a habit tracker.
 '''),
   GenericItem(
       label: 'AbbyCastillo.dev',
       mainText: 'It\'s the little guy at the bottom of your screen!',
-      expandedText: 'A work in progress. I promise I\'m working on it.'),
+      expandedText:
+          'The website you are currently visiting. :) Developed in **Flutter** and **Dart** for **web browsers**.'),
   GenericItem(
       label: 'The Extent of Our Knowledge',
-      expandedText: 'Data visualization made with MAX M/S/P.'),
+      expandedText:
+          'Data visualization/sonification made with MAX M/S/P. Created as a project during the Creative Coding & Immersive Technologies program at Portland Community College.'),
   GenericItem(
     label: 'Education',
-    expandedText: '''Ada Developers Academy
+    expandedText: '''**Ada Developers Academy**, full-stack web development
 
-Portland Community College
+**Portland Community College**, Creative Coding & Immersive Technologies.
 
-Reed College
+**Reed College**, Anthropology
 ''',
   )
 ];
