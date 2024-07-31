242b6869306d45a7ee62a5d557f19974968341b3
abbymachines <abby.machines@gmail.com>
2024-07-31 15:43:01 -0700

    add 'wormchat' portfolio item

--- lib/data/portfolio_items_data.dart
+++ lib/data/portfolio_items_data.dart
@@ -12,10 +12,12 @@ final items = [
   I am a full-stack software engineer and interdisciplinary artist based in Portland, OR.
 
   email: abby.machines@gmail.com
+
+  GitHub: https://github.com/abbymachines
   
   LinkedIn: https://www.linkedin.com/in/abbycc/
   
-  Artist C.V.: https://abbymachines.art
+  Artist C.V.: https://abbymachines.art/cv
 ''',
     color: const Color.fromARGB(255, 255, 195, 15),
     style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
@@ -83,6 +85,11 @@ When habits are incomplete, the pet gets hungry and sick. Completing habits feed
       expandedText:
           'Data visualization/sonification made with MAX M/S/P. Created as a project during the Creative Coding & Immersive Technologies program at Portland Community College. Documents weekly COVID deaths from 2020-2022 as audio and visuals. [Video available on YouTube.](https://www.youtube.com/watch?v=xNiid90HxCQ)',
       color: const Color.fromARGB(255, 255, 252, 252)),
+  GenericItem(label: 'wormchat', expandedText: '''
+![screenshot of wormchat](assets/wormchat0.png)
+
+Part of **One Thousand Machines v1.1**. Excerpted [from abbymachines.art](https://abbymachines.art/A/OneThousandMachines_v1-1/): *2022.5.2. This one is in lieu of my April report. I thought this time I would just embed wormchat (v1), my first attempt at an interactive chatbot. Building this has taught me a bit more about Markov chains, though I am still trying to wrap my head around them. You can see the source code (along with comments) at the following link: https://abbymachines.art/A/OneThousandMachines_v1-1/wormchat_v1/sketch.js.*
+          '''),
   GenericItem(
     label: 'Education',
     expandedText: '''**Ada Developers Academy**, full-stack web development
